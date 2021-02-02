94603ed42e12f500d76880064f214d44a3be4839
kauwai <53876126+kauwai@users.noreply.github.com>
2021-02-02 15:16:28 -0300

    Change to http

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://grades-app-kauwai.herokuapp.com',
+    origin: 'http://grades-app-kauwai.herokuapp.com',
   })
 );
 
